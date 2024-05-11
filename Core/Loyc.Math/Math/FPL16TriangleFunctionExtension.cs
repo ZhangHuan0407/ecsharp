8e1b4cd29e8cc3dda43cbc4da5fce5fb6cab5969
Zhang Huan <2360950578@qq.com>
2024-05-11 21:30:01 +0800

    add Vector2FPL16, FPL16TriangleFunciton

---
+++ Core/Loyc.Math/Math/FPL16TriangleFunctionExtension.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Loyc.Math
+{
+    public static class FPL16TriangleFunctionExtension
+    {
+        public const long PI_Prescaled = 205887;
+        // double: 3.1415926, fp: 3.9279
+        public static readonly FPL16 PI = FPL16.Prescaled(205887);
+        public static readonly FPL16 PI2 = PI * 2L;
+        public static readonly FPL16 TUO = PI / 2L;
+
+        // double: 2.718281828, fp: 2.47073
+        public static readonly FPL16 E = FPL16.Prescaled(178145);
+        // 0.001
+        private const long Interval_Prescaled = 66;
+        private static readonly long[] SinFactorialTable;
+        private static readonly long[] ArcSinFactorialTable;
+
+        private static readonly FPL16 _0_93 = FPL16.Prescaled(60948);
+        private static readonly FPL16 _1_649047 = FPL16.Prescaled(108072);
+        private static readonly FPL16 _0_14083 = FPL16.Prescaled(9229);
+        private static readonly FPL16 _0_86 = FPL16.Prescaled(56361);
+        private static readonly FPL16 _0_02 = FPL16.Prescaled(1311);
+
+        static FPL16TriangleFunctionExtension()
+        {
+            SinFactorialTable = new long[5];
+            long Factorial = 1;
+            for (int i = 0; i < SinFactorialTable.Length; i++)
+            {
+                Factorial *= (i * 2 + 2) * (i * 2 + 3);
+                SinFactorialTable[i] = Factorial;
+                if (i % 2 == 0)
+                    SinFactorialTable[i] = -SinFactorialTable[i];
+            }
+
+            ArcSinFactorialTable = new long[5];
+            for (int i = 0; i < ArcSinFactorialTable.Length; i++)
+            {
+                long numerator = 1;
+                for (int j = 0; j < i; j++)
+                    numerator *= 2 * j + 3;
+                long denominator = 1;
+                for (int j = 0; j < i + 1; j++)
+                    denominator *= 2 * (j + 1);
+                denominator *= 2 * i + 3;
+                ArcSinFactorialTable[i] = (denominator << 16) / numerator;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 Sin(this MathFPL16 math, FPL16 round)
+        {
+            // round 先约分到一个周期内，这样加快运算速度且后续直接long乘法不溢出
+            long roundLong = round.N;
+            if (roundLong > PI_Prescaled)
+                roundLong -= PI_Prescaled * 2;
+            else if (roundLong < -PI_Prescaled)
+                roundLong += PI_Prescaled * 2;
+            round.N = roundLong;
+
+            int circleTimes = 0;
+            long delta;
+            FPL16 p = round;
+            FPL16 taylorExpansion = round;
+            FPL16 roundSquare = FPL16.Prescaled((round.N * round.N) >> 16);
+            do
+            {
+                p.N = (p.N * roundSquare.N) >> 16;
+                delta = p.N / SinFactorialTable[circleTimes];
+                taylorExpansion.N += delta;
+            } while (++circleTimes < 5 && (delta > 0 ? delta : -delta) > Interval_Prescaled);
+            return taylorExpansion;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 Cos(this MathFPL16 math, FPL16 round)
+        {
+            round.N = TUO.N - round.N;
+            return math.Sin(round);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 Tan(this MathFPL16 math, FPL16 round)
+        {
+            return math.Sin(round) / math.Cos(round);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 ArcSin(this MathFPL16 math, FPL16 value, bool precisionCompensation = false)
+        {
+            bool minus = value.N < 0;
+            if (value == FPL16.MinValue)
+                value = FPL16.MaxValue;
+            else if (minus)
+                value.N = -value.N;
+            int circleTimes = 0;
+            FPL16 p = value;
+            FPL16 taylorExpansion = value;
+            FPL16 square = FPL16.Prescaled((value.N * value.N) >> 16);
+            long delta;
+            do
+            {
+                p.N = (p.N * square.N) >> 16;
+                delta = (p.N << 16) / ArcSinFactorialTable[circleTimes];
+                taylorExpansion.N += delta;
+            } while (++circleTimes < 5 && (delta > 0 ? delta : -delta) > Interval_Prescaled);
+
+            if (precisionCompensation)
+            {
+                if (value.N > _0_93.N)
+                {
+                    taylorExpansion.N += (_1_649047.N * math.Ln(value).N >> 16) + _0_14083.N;
+                    if (taylorExpansion.N > TUO.N)
+                        taylorExpansion.N = TUO.N;
+                }
+                else if (value.N > _0_86.N)
+                {
+                    taylorExpansion.N += (value.N - _0_86.N) * _0_02.N / (_0_93.N - _0_86.N);
+                }
+            }
+            if (minus)
+                taylorExpansion.N = -taylorExpansion.N;
+            return taylorExpansion;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 ArcCos(this MathFPL16 math, FPL16 value)
+        {
+            FPL16 result = math.ArcSin(value);
+            result.N = TUO.N - result.N;
+            return result;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 ArcTan2(this MathFPL16 math, FPL16 y, FPL16 x)
+        {
+            bool smallX = x.N > -Interval_Prescaled && x.N < Interval_Prescaled;
+            if (smallX)
+            {
+                if (y.N == 0)
+                    return FPL16.Zero;
+                else if (y.N > 0)
+                    return TUO;
+                else
+                    return -TUO;
+            }
+
+            FPL16 q = FPL16.Zero;
+            q.N = (y.N * y.N + x.N * x.N) >> 16;
+            FPL16 sin = math.Div(y, math.Sqrt(q));
+            return ArcSin(math, sin, true);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 Ln(this MathFPL16 math, FPL16 value, ushort error = 328)
+        {
+            if (value.N < 0)
+                throw new ArgumentException(value.ToString());
+            else if (value.N == 0)
+                return FPL16.MinValue;
+            // y 永远在[-1, 1]
+            FPL16 y = (value - FPL16.One) / (value + FPL16.One);
+            FPL16 taylorExpansion = FPL16.One;
+            FPL16 roundSquare;
+            roundSquare = FPL16.Prescaled((y.N * y.N) >> 2); // 16 + 16 - 2
+
+            FPL16 s = FPL16.Prescaled(1L << 30);
+            for (int i = 0; i < 50; i++)
+            {
+                s.N = (s.N * roundSquare.N) >> 30; // 30 + 30 - 30
+                long delta = (s.N / (i + i + 3)) >> 14; // 30 - 0 - 14
+                taylorExpansion.N += delta;
+                if (delta <= error)
+                    break;
+            }
+            return taylorExpansion * FPL16.Prescaled(y.N + y.N);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 Clamp01(this MathFPL16 math, FPL16 value) => Clamp(math, value, FPL16.Zero, FPL16.One);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FPL16 Clamp(this MathFPL16 math, FPL16 value, FPL16 min, FPL16 max)
+        {
+            if (min.N > max.N)
+                throw new ArgumentException($"min: {min}, max: {max}, min > max");
+            if (value.N < min.N)
+                return min;
+            else if (value.N > max.N)
+                return max;
+            else
+                return value;
+        }
+    }
+}
---
+++ Core/Loyc.Math/Math/Vector2FPL16.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Loyc.Math
+{
+    public struct Vector2FPL16
+    {
+        public static readonly Vector2FPL16 Zero = new Vector2FPL16(FPL16.Zero, FPL16.Zero);
+        private const long DotZeroOne_Prescaled = 655;
+        private const long _10_Prescaled = 655360;
+
+        public FPL16 x;
+        public FPL16 y;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector2FPL16(FPL16 x, FPL16 y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector2FPL16(Vector2Int vector2Int)
+        {
+            x = vector2Int.x;
+            y = vector2Int.y;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 Prescaled(long x, long y)
+        {
+            Vector2FPL16 result;
+            result.x.N = x;
+            result.y.N = y;
+            return result;
+        }
+
+        public FPL16 Magnitude
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (x * x + y * y).Sqrt();
+        }
+        public FPL16 SqrMagnitude
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => x * x + y * y;
+        }
+
+        public Vector2FPL16 Normalized
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                Vector2FPL16 result = this;
+                result.Normalize();
+                return result;
+            }
+        }
+
+        public Vector2 ToVector2
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => new Vector2((float)x, (float)y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Abs()
+        {
+            x.N = x.N < 0 ? -x.N : x.N;
+            y.N = y.N < 0 ? -y.N : y.N;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Normalize()
+        {
+            long sumAbs = (x.N < 0 ? -x.N : x.N) + (y.N < 0 ? -y.N : y.N);
+            if (sumAbs > _10_Prescaled)
+            {
+                FPL16 magnitude = Magnitude;
+                this /= magnitude;
+            }
+            else if (sumAbs > DotZeroOne_Prescaled)
+            {
+                long sqr = x.N * x.N + y.N * y.N;
+                x.N = (x.N << 16) / sqr;
+                y.N = (y.N << 16) / sqr;
+            }
+            else
+                this = Zero;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Vector2FPL16 left, Vector2FPL16 right) => left.x.N == right.x.N && left.y.N == right.y.N;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Vector2FPL16 left, Vector2FPL16 right) => left.x.N != right.x.N || left.y.N != right.y.N;
+        public override int GetHashCode() => ((int)x.N ^ (int)(x.N >> 32)) ^ (((int)y.N ^ (int)(y.N >> 32)) << 2);
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Vector2FPL16 value)
+                return this == value;
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 operator +(Vector2FPL16 left, Vector2FPL16 right) => Prescaled(left.x.N + right.x.N, left.y.N + right.y.N);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 operator -(Vector2FPL16 left, Vector2FPL16 right) => Prescaled(left.x.N - right.x.N, left.y.N - right.y.N);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 operator *(Vector2FPL16 a, FPL16 d) => new Vector2FPL16(a.x * d, a.y * d);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 operator /(Vector2FPL16 a, FPL16 d) => new Vector2FPL16(a.x / d, a.y / d);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 operator *(Vector2FPL16 a, int d) => Prescaled(a.x.N * d, a.y.N * d);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2FPL16 operator /(Vector2FPL16 a, int d) => Prescaled(a.x.N / d, a.y.N / d);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override string ToString() => $"({x}, {y})";
+    }
+}
